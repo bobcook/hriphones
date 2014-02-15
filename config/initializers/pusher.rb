c5d23edaf3b1c0d68c029bf5cd0ed4cc4099a575
Bob Cook <bobcook@sunrem.com>
2014-02-14 21:58:05 -0700

    pusher stuff

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,22 +1,22 @@
 require 'pusher'
 
-unless defined?(PUSHER_CONFIG) && PUSHER_CONFIG
-  filename = 'config/pusher.yml'
-  if File.exists? Rails.root.join(filename)
-    pusher_configs = YAML.load_file Rails.root.join(filename)
-    PUSHER_CONFIG = pusher_configs[Rails.env]
-  else
-    PUSHER_CONFIG = nil
-  end
-end
+#unless defined?(ENV['PUSHER_APP_ID']) #&& PUSHER_CONFIG
+  #filename = 'config/pusher.yml'
+  #if File.exists? Rails.root.join(filename)
+    #pusher_configs = YAML.load_file Rails.root.join(filename)
+   # PUSHER_CONFIG = pusher_configs[Rails.env]
+  #else
+    #PUSHER_CONFIG = nil
+    #end
+#end
 
-if PUSHER_CONFIG
-  Pusher.app_id = PUSHER_CONFIG["app_id"]
-  Pusher.key = PUSHER_CONFIG["app_key"]
-  Pusher.secret = PUSHER_CONFIG["secret"]
-else
-  Rails.logger.warn "Telephony will not work. #{filename} does not include config for RAILS_ENV=#{Rails.env}"
-end
+#if PUSHER_CONFIG
+  #Pusher.app_id = PUSHER_CONFIG["app_id"]
+  #Pusher.key = PUSHER_CONFIG["app_key"]
+  #Pusher.secret = PUSHER_CONFIG["secret"]
+  #else
+  #Rails.logger.warn "Telephony will not work. #{filename} does not include config for RAILS_ENV=#{Rails.env}"
+  #end
 
 
 # test if you have ENV var, load from file if not
